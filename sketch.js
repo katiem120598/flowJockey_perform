f55cd68755288319c22a32646e052e3b370981e5
Katie Mueller <63931373+katiem120598@users.noreply.github.com>
2025-11-20 13:52:55 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+let points = [];
+let shapes = [];
+let normpoints = [];
+let normshapes = [];
+let clientnum = 0;
+let mode = "grid"; // Default mode
+const serverAddress = "wss://serverfortwo.glitch.me/";
+const ws = new WebSocket(serverAddress);
+const bass = document.getElementById("bass");
+const mid = document.getElementById("mid");
+const treble = document.getElementById("treble");
+const playpause = document.getElementById("playButton")
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Attach the event listener to each radio button with the name 'mode'
+  const startDraw = document.createElement("text")
+  const modeOptions = document.getElementsByName("mode");
+  modeOptions.forEach(function (radio) {
+    radio.addEventListener("change", function () {
+      if (this.checked) {
+        mode = this.value;
+        console.log("Selected mode:", mode);
+
+        // Send the mode change to the server
+        const modeswitch = { type: "modeswitch", mode: mode };
+        ws.send(JSON.stringify(modeswitch));
+      }
+    });
+  });
+});
+
+bass.addEventListener("input", function () {
+  console.log("Current slider value:", bass.value);
+  const modeswitch = { type: "bassval", val:bass.value};
+  ws.send(JSON.stringify(modeswitch));
+  // Update some real-time display or effect
+});
+
+mid.addEventListener("input", function () {
+  // Update some real-time display or effect
+  const modeswitch = { type: "midval",val:mid.value };
+  ws.send(JSON.stringify(modeswitch));
+});
+treble.addEventListener("input", function () {
+  const modeswitch = { type: "trebleval", val:treble.value };
+  ws.send(JSON.stringify(modeswitch));
+  // Update some real-time display or effect
+});
+playpause.addEventListener("click", function () {
+  const playpause = { type: "playpause", val:"pressed" };
+  ws.send(JSON.stringify(playpause));
+  // Update some real-time display or effect
+});
+
+// WebSocket connection setup
+ws.onopen = function () {
+  // Send initial client info when the WebSocket connection opens
+  const clientdata = { type: "client_info", app: "perform" };
+  ws.send(JSON.stringify(clientdata));
+  console.log("I just connected to the server on " + serverAddress);
+};
+
+// Handling messages from server
+ws.onmessage = function (event) {
+  // Process server messages here
+  console.log("Message from server:", event.data);
+};
